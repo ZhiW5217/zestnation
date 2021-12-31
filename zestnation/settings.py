f532feee3411bd50ec8cbd6ba7ae37e733ef0b00
Cx330 <zhiwei5217@126.com>
2021-12-31 17:45:43 +0800

    设置页面缓存

--- zestnation/settings.py
+++ zestnation/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'zestnation.urls'
@@ -207,3 +209,10 @@ SIMPLEUI_ICON = {
     '新闻': 'far fa-newspaper'
 }
 SIMPLEUI_DEFAULT_THEME = 'highdmin.css'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
